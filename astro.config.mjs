f054096540c8a5d233cd971073a079164894b5c7
Wilfried <Wilfried.lafaye@icloud.com>
2026-03-03 15:39:49 +0100

    fix: set production site URL in astro config to fix OG/Twitter meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://wilfried-lafaye.fr',
     outDir: './docs',
     build: {
         format: 'file'
